0108d1d5ca2e660decbced0c4a0394a626e21328
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2024-10-21 10:04:13 -0400

    AddtoList-kinda

--- Grocery-list.js
+++ Grocery-list.js
@@ -1,20 +1,10 @@
-ready()
-function ready (){
-    let addBtn = document.getElementsByClassName('shop-btn')
-    for(let i = 0; i < addBtn.length;i++){
-        let button = addBtn[i]
-        button.addEventListener('click',addbtnclicked )
-    }
-}
-
-function addbtnclicked(event){
-    let button = event.target;
-    let item = button.parentElement.parentElement;
-    let itemTitle = item.getElementsByClassName('item-title')
-additemtoList(itemTitle)    
-}
-function additemtoList(item{
-    let listContainer = document.getElementsByClassName('list-container')
-    let listItemName = document.getElementsByClassName('listItem-title')
-    let ListrowContent = ""
-}
+addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    let search = document.getElementsByClassName("SearchBar");
+    let list = document.getElementsByClassName("list");
+    let itemSearched = search[0].value;
+    let listColumn = document.createElement("li");
+    let listItem = (listColumn.innerHTML = itemSearched);
+    list[0].append(listItem);
+  }
+});
